92268a4bdc98780b85b51d834e0e952876c6df6e
fabadillam <felipe.badilla@ucr.ac.cr>
2022-07-15 01:57:38 -0600

    Visualización de datos y resultados, versión final y estable

--- Source/Code/Master.py
+++ Source/Code/Master.py
@@ -1,7 +1,8 @@
+# Archivo para aumatizar la lectura de bases de datos locales
 import pandas as pd
-import sys
 import os
 import time
+# Medimos tiempo de ejecusión
 inicio = time.time()
 
 DATA_DIR = "\Raw_Data"
@@ -24,41 +25,6 @@ for inputDB in Inputs_DB:
     print('Input: ', input)
     os.system(Preprocessing_file + ' ' + input + ' ' + output + ' ' + balance)
 
-print("Merging results")
-df1 = pd.read_excel("../Results/T_out1.xlsx")
-df2 = pd.read_excel("../Results/T_out2.xlsx")
-df3 = pd.read_excel("../Results/T_out3.xlsx")
-df4 = pd.read_excel("../Results/T_out4.xlsx")
-df5 = pd.read_excel("../Results/T_out5.xlsx")
-df6 = pd.read_excel("../Results/T_out6.xlsx")
-df7 = pd.read_excel("../Results/T_out7.xlsx")
-df8 = pd.read_excel("../Results/T_out8.xlsx")
-df9 = pd.read_excel("../Results/T_out9.xlsx")
-df10 = pd.read_excel("../Results/T_out10.xlsx")
-df11 = pd.read_excel("../Results/T_out11.xlsx")
-df12 = pd.read_excel("../Results/T_out12.xlsx")
-df13 = pd.read_excel("../Results/T_out13.xlsx")
-df14 = pd.read_excel("../Results/T_out14.xlsx")
-
-df_final = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14])
-
-# creating a new excel file and save the data
-df_final.to_excel("../Results/ML_input.xlsx", index=False)
-
-# os.remove("../Results/T_out1.xlsx")
-# os.remove("../Results/T_out2.xlsx")
-# os.remove("../Results/T_out3.xlsx")
-# os.remove("../Results/T_out4.xlsx")
-# os.remove("../Results/T_out5.xlsx")
-# os.remove("../Results/T_out6.xlsx")
-# os.remove("../Results/T_out7.xlsx")
-# os.remove("../Results/T_out8.xlsx")
-# os.remove("../Results/T_out9.xlsx")
-# os.remove("../Results/T_out10.xlsx")
-# os.remove("../Results/T_out11.xlsx")
-# os.remove("../Results/T_out12.xlsx")
-# os.remove("../Results/T_out13.xlsx")
-# os.remove("../Results/T_out14.xlsx")
 fin = time.time()
 print("Finished successfully!!")
 print("Tiempo de ejecución: ", fin-inicio)
